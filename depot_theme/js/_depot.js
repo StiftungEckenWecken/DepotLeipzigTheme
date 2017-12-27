77fb15796b51c8c37b4164b7a980a3d8fda916d0
Lukas Speck <lspeck@gmx.de>
2017-12-27 16:48:39 +0100

    css fixes for user pages + follow-box with style and function for ressource-detail page

--- depot_theme/js/_depot.js
+++ depot_theme/js/_depot.js
@@ -1,45 +1,71 @@
 /* Depot javascript */
 
 ;(function ($, window, document, undefined) {
-  'use strict';
+    'use strict';
 
-  $(document).ready(function(){
+    $(document).ready(function () {
 
-    $('#depot-global-select-kategorie').change(function(){
-      var targetKategorie = $(this).val();
-      var target = '/ressourcen';
-      
-      if (targetKategorie !== '') {
-          target += '?kategorie_id=' + targetKategorie;
-      }
+        $('#depot-global-select-kategorie').change(function () {
+            var targetKategorie = $(this).val();
+            var target = '/ressourcen';
 
-      window.location = target;  
+            if (targetKategorie !== '') {
+                target += '?kategorie_id=' + targetKategorie;
+            }
+
+            window.location = target;
+        });
+
+        $('#edit-field-links-i input[type="text"]').keyup(function () {
+            if ($(this).val().length >= 1) {
+                $('#edit-field-links-ii').removeClass('hide');
+            }
+        });
+
+        $('#edit-field-links-ii input[type="text"]').keyup(function () {
+            if ($(this).val().length >= 1) {
+                $('#edit-field-links-iii').removeClass('hide');
+            }
+        });
+
+        $('.fieldset-toggle legend').click(function () {
+            $(this).closest('.fieldset-toggle').toggleClass('toggled');
+        });
+
+        if ($('body').hasClass('section-ressourcen')) {
+            var url = window.location.pathname;
+            if (url.search('av') > 1) {
+                $('#verfuegbarkeitenModal').trigger('click');
+            }
+
+        }
+
+        var $followBoxes = $('.follow-box');
+        if ($followBoxes.length > 0) {
+            var followBoxes = [];
+
+            for (var i = 0; i < $followBoxes.length; i++) {
+                followBoxes.push({
+                    element: $followBoxes[i],
+                    offset: $($followBoxes[i]).offset()
+                });
+            }
+
+            $(window).scroll(function (e) {
+                var scrollTop = $(e.currentTarget).scrollTop();
+
+                for (var i = 0; i < followBoxes.length; i++) {
+
+                    if(scrollTop > followBoxes[i].offset.top) {
+                        $(followBoxes[i].element).addClass('follow');
+                    } else {
+                        $(followBoxes[i].element).removeClass('follow');
+                    }
+                }
+
+            });
+        }
+
     });
 
-    $('#edit-field-links-i input[type="text"]').keyup(function(){
-      if ($(this).val().length >= 1){
-        $('#edit-field-links-ii').removeClass('hide');
-      }
-    });
-
-    $('#edit-field-links-ii input[type="text"]').keyup(function(){
-      if ($(this).val().length >= 1){
-        $('#edit-field-links-iii').removeClass('hide');
-      }
-    });
-
-    $('.fieldset-toggle legend').click(function(){
-      $(this).closest('.fieldset-toggle').toggleClass('toggled');
-    });
-
-    if ($('body').hasClass('section-ressourcen')){
-      var url = window.location.pathname;
-      if (url.search('av') > 1){
-        $('#verfuegbarkeitenModal').trigger('click');
-      }
-
-    }
-
-}); 
-  
 }(jQuery, window, window.document));
